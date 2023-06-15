cb77d4c50e3b9438109a2ebd2c6309cb655bde21
Adam McAleer <amcaleer1127@gmail.com>
2023-06-14 22:02:58 -0400

    Created stand up test, not yet working

--- Arduino Code/servoTesting/servoTesting.ino
+++ Arduino Code/servoTesting/servoTesting.ino
@@ -12,11 +12,11 @@ void setup() {
     joint[i].attach(21 + i);
   }
   Serial.println("Servos Attached");
+  delay(1000);
+  standTest();
 }
 
 void loop() {
-
-
   
 
 }
@@ -28,6 +28,7 @@ void moveAll(int pos){
    }
 }
 
+//prompted to enter a servo to test, then rotates 50 degrees back/forth
 void individualTest(){
   Serial.println("Enter joint to test: ");
   while(Serial.available() == 0){
@@ -43,3 +44,21 @@ void individualTest(){
     delay(20);
   }
 }
+
+void standTest(){
+
+  Serial.println("Standing...");
+
+  for(int i = 1; i <= 100; i++){
+    joint[23].write(49 + i);
+    joint[20].write(49 + i);
+    joint[17].write(49 + i);
+
+    joint[2].write(101 - i);
+    joint[5].write(101 - i);
+    joint[8].write(101 - i);
+
+    delay(30);
+  }
+}
+
