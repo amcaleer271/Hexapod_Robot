cf89995b80cd7a7637642736d8df259735862def
Adam McAleer <amcaleer1127@gmail.com>
2023-06-23 21:57:53 -0400

    Created primitive walk cycle

--- Arduino Code/servoTesting/servoTesting.ino
+++ Arduino Code/servoTesting/servoTesting.ino
@@ -40,24 +40,72 @@ void setup() {
 }
 
 void loop() {
-  
+  walkf(1,38, 3.14, 0);
 }
 
-void walkf(int period, int steps){
-  for(int i = 0; i <= steps; i++){
-    int group1 = i;
-    int group2 = i + steps/2;
-    if(group2 > steps){
-      group2 = group2 % steps;
-    }
-    move11(group1);
-    move21(group2);
-    move31(group1);
+//params - period [s], steps [steps], offset [rads], enable [0/1]
+void walkf(float period, int steps, float shift, int enable){
+  float group1;
+  float group2;
+  float group1Hip;
+  float group2Hip;
+  float group1Knee;
+  float group2Knee;
 
-    move61(-1 * group2);
-    move51(-1 * group1);
-    move41(-1 * group2);
-    delay(1000 * period / steps);
+  for(int i = 0; i <= 2 * steps; i++){
+    int hStep = steps/2;
+    group1 = hStep + hStep * cos(2 * 3.14 * i / (2*steps));
+    group2 = hStep + hStep * cos((2 * 3.14 * i / (2*steps)) + shift);
+
+    group1Hip = group1;
+    group2Hip = group2;
+
+    if((i % (2 * steps)) < steps){
+      group1Knee = 180;
+      group2Knee = 180 - 2 *(hStep + hStep * cos((2 * 3.14 * i / steps) + shift));
+    }
+    else{
+      group1Knee = 180 - 2 * (hStep + hStep * cos((2 * 3.14 * i / steps) + shift));
+      group2Knee = 180;
+    }
+    
+    //move all limbs
+    if(enable == 1){
+      //upper limbs
+      move11(group1Hip + 30);
+      move21(group2Hip + 30);
+      move31(group1Hip + 55);
+
+      move61(-1 * group2Hip - 30);
+      move51(-1 * group1Hip - 30);
+      move41(-1 * group2Hip - 55);
+
+      //middle limbs
+      move12(group1Knee);
+      move22(group2Knee);
+      move32(group1Knee);
+
+      move62(-1 * group2Knee);
+      move52(-1 * group1Knee);
+      move42(-1 * group2Knee);
+
+      //end limbs
+    }
+
+    //debug info
+    Serial.print("i: ");
+    // Serial.print(i);
+    // Serial.print(" , group1: ");
+    // Serial.print(group1);
+    // Serial.print(" , group2: ");
+    // Serial.print(group2);
+    Serial.print(" , group1Knee: ");
+    Serial.print(group1Knee);
+    Serial.print(" , group2Knee: ");
+    Serial.println(group2Knee);
+    
+
+    delay(float(1000 * period / steps));
   }
 
 }
