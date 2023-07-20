4eccebe6ee8ae0b0afc6a89df2a52a9bdcdb8e0b
Adam McAleer <amcaleer1127@gmail.com>
2023-07-19 22:50:49 -0400

    Created cap part

--- Arduino Code/hexapodMain/hexapodMain.ino
+++ Arduino Code/hexapodMain/hexapodMain.ino
@@ -70,11 +70,92 @@ void loop() {
   }
   //Serial.println(walkStep);
   controllerWalk(walkStep, 1, 38, 0, 1);
-  delay(20);
+  delay(25);
   
 }
 
+void controllerTurn(int currentStep, int period, int steps, int direction, int enable){
+  float group1;
+  float group2;
+  float group1Hip;
+  float group2Hip;
+  float group1Knee;
+  float group2Knee;
+  float group1Foot;
+  float group2Foot;
+  int i = currentStep;
+  int hStep = steps/2;
 
+  //calculate hip angles
+  group1 = hStep + hStep * cos(2 * 3.14159 * i / (2*steps));
+  group2 = hStep + hStep * cos((2 * 3.14159 * i / (2*steps)) + 3.14159);
+
+  group1Hip = group1;
+  group2Hip = group2;
+
+  //calculate knee angles
+  if((i % (2 * steps)) < steps){
+    group1Knee = 180;
+    group2Knee = 180 - 2 *(hStep + hStep * cos((2 * 3.14159 * i / steps) + 3.14159));
+  }
+  else{
+    group1Knee = 180 - 2 * (hStep + hStep * cos((2 * 3.14159 * i / steps) + 3.14159));
+    group2Knee = 180;
+  }
+  
+  //calculate foot angles
+  if(group1Knee == 180){
+    group1Foot = 0;
+    group2Foot = 180 - group2Knee;
+  }
+  else{
+    group1Foot = 180 - group1Knee;
+    group2Foot = 0;
+  }
+  if(enable == 1){
+    //upper limbs
+    move11(1.1*group1Hip + 30);
+    move21(1.1*group2Hip + 30);
+    move31(1.1*group1Hip + 55);
+
+    move61(-1 * 0.7*group2Hip - 30);
+    move51(-1 * 0.7*group1Hip - 30);
+    move41(-1 * 0.7*group2Hip - 55);
+
+    //middle limbs
+    move12(group1Knee);
+    move22(group2Knee);
+    move32(group1Knee);
+
+    move62(-1 * group2Knee);
+    move52(-1 * group1Knee);
+    move42(-1 * group2Knee);
+
+    //end limbs        
+    move13(-1 * group1Foot + 110);
+    move23(-1 * group2Foot + 110);
+    move33(-1 * group1Foot + 110);
+
+    move63(group2Foot - 110);
+    move53(group1Foot - 110);
+    move43(group2Foot - 110);
+  }
+  //debug info
+  // Serial.print("i: ");
+  // Serial.print(i);
+  // Serial.print(" , group1: ");
+  // Serial.print(group1);
+  // Serial.print(" , group2: ");
+  // Serial.print(group2);
+  // Serial.print(" , group1Knee: ");
+  // Serial.print(group1Knee);
+  // Serial.print(" , group2Knee: ");
+  // Serial.print(group2Knee);
+  // Serial.print(" , group1Foot: ");
+  // Serial.print(group1Foot);
+  // Serial.print(" , group2Foot: ");
+  // Serial.println(group2Foot);
+}
 void controllerWalk(int currentStep, int period, int steps, int direction, int enable){
   float group1;
   float group2;
